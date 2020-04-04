6afa426ec6279ada470adaf636c4c824f368088f
Creeposuc <39770050+Creeposuc@users.noreply.github.com>
2020-04-04 22:03:26 +0200

    1.1 - recherche csv et datetime

--- test_CSV.py
+++ test_CSV.py
@@ -1,3 +1,10 @@
 import csv
-for i in range(10):
-    pass
+import datetime
+import time
+with open("valeurtest.csv", "a") as csvfile:
+    ecrire = csv.writer(csvfile, delimiter=" ")
+    for i in range(10):
+        heure = datetime.datetime.now()
+        print(f"{heure.hour}:{heure.minute}:{heure.second}")
+        time.sleep(2)
+        ecrire.writerow(f"{i},°C")
